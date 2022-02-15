34b9ad361b749246d5242a02cf7d49810091fdda
Salmanabdullah <salmandcc@gmail.com>
2022-02-15 17:02:50 +0100

    ..

---
+++ booklist/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const books = [
+  {
+    img:'https://m.media-amazon.com/images/I/71F14G1CqBL._AC_UY218_.jpg',
+    title:'Data Science for Beginners: 4 books in 1',
+    author:'Andrew Park '
+  },
+  {
+    img:'https://m.media-amazon.com/images/I/71nqa-DTIyL._AC_UY218_.jpg',
+    title:'AI Crash Course',
+    author:'Hadelin de Ponteves '
+  },
+  {
+    img:'https://m.media-amazon.com/images/I/71x8gdSJVCL._AC_UY218_.jpg',
+    title:'Hands-On Data Science and Python Machine Learning',
+    author:'Frank Kane '
+  },
+  {
+    img:'https://images-na.ssl-images-amazon.com/images/I/510V86isnRL._SX404_BO1,204,203,200_.jpg',
+    title:'Hands-On Machine Learning with scikit-learn and Scientific Python Toolkits',
+    author:'Tarek Amr '
+  }
+];
+
+const Booklist = () => {
+  return (
+    <section>
+      {books.map(item =>{
+        const {img,title,author} = item;
+        return <Book item={item}/>
+      })}
+    </section> 
+  );
+}
+
+function Book(props){
+  console.log(props);
+  const {img,title,author} = props.item;
+  return (
+    <article>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h3>{author}</h3>
+    </article>
+  );
+}
+
+ReactDOM.render(<Booklist/>, document.getElementById('root'));
+
