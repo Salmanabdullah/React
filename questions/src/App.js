17798e85a322e53641bf71a49ff310a2339c02d1
Salmanabdullah <salmandcc@gmail.com>
2022-03-13 17:53:12 +0100

    updated

--- questions/src/App.js
+++ questions/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import questions from "./data";
-import SingleQuestion from "./question"
+import SingleQuestion from "./question";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h3>questions and answers about login</h3>
-      <section>
-        {questions.map((question) => {
-          return(
-            <SingleQuestion key={question.id} {...question} />
-          )
-        })}
-      </section>
+    <div className="d-flex justify-content-between text-capitalize box">
+      <div className="p-2">
+        <h3 className="mt-5 py-5 px-1">questions and answers about login</h3>
+      </div>
+      <div className="w-100">
+        <section>
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
     </div>
   );
 }
--- questions/src/question.js
+++ questions/src/question.js
@@ -4,17 +4,22 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 // on click the answers will be visible means showInfo needs to be true
 // then the button will change based on showInfo
 // text will be displayed togather with the button
-const Question = ({ title, info }) => {
+const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
   return (
-    <article>
-      <h4>{title} </h4>
-      <button onClick={() => setShowInfo(!showInfo)}>
-        {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
-      </button>
-      {showInfo && <p>{info}</p>}
+    <article className="my-4">
+      <div className=" d-flex justify-content-between mx-4" >
+        <h4 className="fw-bold">{title} </h4>
+        <button
+          onClick={() => setShowInfo(!showInfo)}
+          className="rounded-circle pb-1 text-danger"
+        >
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </div>
+      {showInfo && <p className="mx-4">{info}</p>}
     </article>
   );
 };
 
-export default Question;
+export default SingleQuestion;
